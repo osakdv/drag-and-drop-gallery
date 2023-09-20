27ebc48c40c70b649b866615d1a711e52671bb84
osakdv <osakdv@gmail.com>
2023-09-20 14:36:56 +0100

    Input:file functionality

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,73 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Imgcard from "./components/Card";
+import './Styles/Home.css'
+
 const Home = () => {
+    const allImages = []
+    
+    const callImgCard = (imgUrl, title, tags) => {
+        return <Imgcard img={imgUrl} title={title} tags={tags}/>
+    }
+
+    const enterImgDetails = () => {
+        
+    }
+    
+    const fileHandler = (file, name, type) => {
+        console.log("clicking")
+        if(type.split("/")[0] !== "image") {
+            return false
+        }
+
+        const reader = new FileReader()
+        reader.readAsDataURL()
+        reader.onloadend = () => {
+            // call the popup
+            // push the call img
+            const imgCard = callImgCard()
+            allImages.push(imgCard)
+        }
+    }
     return(
         <div className="home-page">
             {/* nav */}
             {/* marquee */}
             {/* img content */}
-            <div className="images-holder"></div>
+            <div className="home-content">
+                <div className="add-img-container">
+                    <div className="add-img">
+                        <input type="file" name="add-img" id="add-img" />
+                        <label className="add-img-label" htmlFor="add-img" onClick={fileHandler}>
+                            <FontAwesomeIcon className="add-icon" icon={faPlus} />
+                        </label>
+                    </div>
+                    <small className="message">
+                        Click or drag and drop to add image
+                    </small>
+                </div>
+
+                <div className="card-container">
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                    <Imgcard />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,42 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 const Navbar = () => {
-    return(
-        <div className="home-page"></div>
-    )
-}
+  return (
+    <div className="navbar">
+      {/*  */}
+      <div className="logo-and-links">
+        {/* logo */}
+        <Link to="/">
+          <div className="main-logo">
+            W<span>.</span>
+          </div>
+        </Link>
+
+        {/* filterable links */}
+        <ul>
+          <li>Culture</li>
+          <li>Culture</li>
+          <li>Culture</li>
+          <li>Culture</li>
+        </ul>
+      </div>
+
+      <div className="search-and-signin">
+        {/* seach icon */}
+        <div className="search-field">
+          <input type="text" name="" id="search-input" />
+          <label htmlFor="search-input">
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </label>
+        </div>
+
+        {/* sigin */}
+        <button>Sign in</button>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
